48724537ecdc5b621af4af84f459cd0c36208a58
Biancamoraaes <biancagoesm@gmail.com>
2024-07-31 18:28:58 -0300

    index

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   var startText = document.getElementById("start-text")
   var link = document.createElement("a")
-  link.href = "second-page.html" 
+  link.href = "second-page.html"
   link.textContent = startText.textContent
   link.className = "text"
   startText.parentNode.replaceChild(link, startText)
